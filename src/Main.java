a21df8b954f5c2224b903e434a8ad4734ff28e66
Nick <makarovmykola@gmail.com>
2016-08-06 11:40:50 +0300

    set up postgres at home

--- src/Main.java
+++ src/Main.java
@@ -1,108 +1,50 @@
-import java.sql.*;
-import java.util.logging.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] argv) {
 
-        Connection connection = null;
-        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
-        String url = "jdbc:postgresql://127.0.0.1:5432/test";
-        //Имя пользователя БД
-        String name = "postgres";
-        //Пароль
-        String password = "2005980";
+        System.out.println("-------- PostgreSQL "
+                + "JDBC Connection Testing ------------");
+
         try {
-            //Загружаем драйвер
+
             Class.forName("org.postgresql.Driver");
-            System.out.println("Драйвер подключен");
-            //Создаём соединение
-            connection = DriverManager.getConnection(url, name, password);
-            System.out.println("Соединение установлено");
-            //Для использования SQL запросов существуют 3 типа объектов:
-            //1.Statement: используется для простых случаев без параметров
-            Statement statement = null;
 
-            statement = connection.createStatement();
-            //Выполним запрос
-            ResultSet result1 = statement.executeQuery(
-                    "SELECT * FROM company WHERE age = 25");
-            //result это указатель на первую строку с выборки
-            //чтобы вывести данные мы будем использовать
-            //метод next() , с помощью которого переходим к следующему элементу
-            System.out.println("Выводим statement");
-//            while (result1.next()) {
-//                System.out.println("Номер в выборке #" + result1.getRow()
-//                        + "\t Номер в базе #" + result1.getInt("id")
-//                        + "\t" + result1.getString("username"));
-//            }
-            // Вставить запись
-            statement.executeUpdate(
-                    "INSERT INTO NAME values('name')");
-            //Обновить запись
-            statement.executeUpdate(
-                    "UPDATE user SET username = 'admin' where id = 1");
+        } catch (ClassNotFoundException e) {
 
+            System.out.println("Where is your PostgreSQL JDBC Driver? "
+                    + "Include in your library path!");
+            e.printStackTrace();
+            return;
 
-
-            //2.PreparedStatement: предварительно компилирует запросы,
-            //которые могут содержать входные параметры
-            PreparedStatement preparedStatement = null;
-            // ? - место вставки нашего значеня
-            preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM user where id > ? and id < ?");
-            //Устанавливаем в нужную позицию значения определённого типа
-            preparedStatement.setInt(1, 2);
-            preparedStatement.setInt(2, 10);
-            //выполняем запрос
-            ResultSet result2 = preparedStatement.executeQuery();
-
-            System.out.println("Выводим PreparedStatement");
-            while (result2.next()) {
-                System.out.println("Номер в выборке #" + result2.getRow()
-                        + "\t Номер в базе #" + result2.getInt("id")
-                        + "\t" + result2.getString("username"));
-            }
-
-            preparedStatement = connection.prepareStatement(
-                    "INSERT INTO user(username) values(?)");
-            preparedStatement.setString(1, "user_name");
-            //метод принимает значение без параметров
-            //темже способом можно сделать и UPDATE
-            preparedStatement.executeUpdate();
-
-
-
-            //3.CallableStatement: используется для вызова хранимых функций,
-            // которые могут содержать входные и выходные параметры
-            CallableStatement callableStatement = null;
-            //Вызываем функцию myFunc (хранится в БД)
-            callableStatement = connection.prepareCall(
-                    " { call myfunc(?,?) } ");
-            //Задаём входные параметры
-            callableStatement.setString(1, "Dima");
-            callableStatement.setString(2, "Alex");
-            //Выполняем запрос
-            ResultSet result3 = callableStatement.executeQuery();
-            //Если CallableStatement возвращает несколько объектов ResultSet,
-            //то нужно выводить данные в цикле с помощью метода next
-            //у меня функция возвращает один объект
-            result3.next();
-            System.out.println(result3.getString("MESSAGE"));
-            //если функция вставляет или обновляет, то используется метод executeUpdate()
-
-        } catch (Exception ex) {
-            //выводим наиболее значимые сообщения
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
         }
 
+        System.out.println("PostgreSQL JDBC Driver Registered!");
+
+        Connection connection = null;
+
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost/testdb", "postgres",
+                    "2005980");
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
     }
+
 }
